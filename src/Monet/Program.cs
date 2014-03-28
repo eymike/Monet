0034637335634f5a73851cf6dc48d01bba592b43
mfe <eymike@gmail.com>
2014-03-27 21:56:15 -0700

    Adding form.

---
+++ src/Monet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monet
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MonetCore.MonetApp app = new MonetCore.MonetApp();
+            app.Run();
+        }
+    }
+}
---
+++ src/MonetCore/MonetApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SharpDX;
+using SharpDX.Windows;
+
+namespace MonetCore
+{
+    public class MonetApp
+    {
+        public void Run()
+        {
+            var form = new RenderForm("Monet");
+
+            // Main loop
+            RenderLoop.Run(form, () =>
+            {
+               // context.ClearRenderTargetView(renderView, Color.Black);
+                
+                //swapChain.Present(0, PresentFlags.None);
+            });
+        }
+    }
+}
